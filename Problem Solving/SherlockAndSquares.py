b8708c465bc8cba6c2f9045fd33c628972862c71
Didula Induwara <didulainduwaraamarasinghe@gmail.com>
2023-06-18 22:46:17 +0530

    Add new solution

---
+++ Problem Solving/SherlockAndSquares.py
@@ -0,0 +1,46 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'squares' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER a
+#  2. INTEGER b
+#
+
+def squares(a, b):
+    # Write your code here
+    # squreRoot = pow(a, 0.5)
+    lowerBound = math.floor(pow(a, 0.5))
+    count = 0
+    while pow(lowerBound, 2) <= b:
+        if pow(lowerBound, 2) >= a:
+            count +=1
+        lowerBound +=1
+    return count
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        first_multiple_input = input().rstrip().split()
+
+        a = int(first_multiple_input[0])
+
+        b = int(first_multiple_input[1])
+
+        result = squares(a, b)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
